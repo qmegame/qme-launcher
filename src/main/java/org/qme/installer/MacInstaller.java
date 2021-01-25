0fff397614c55e3fe0daeb0f282bdab664fc098e
adamhutchings <adam.abahot@gmail.com>
2021-01-24 22:21:39 -0800

    Add some isinstalled magic

--- src/main/java/org/qme/installer/MacInstaller.java
+++ src/main/java/org/qme/installer/MacInstaller.java
@@ -1,15 +1,36 @@
 package org.qme.installer;
 
+import java.io.File;
+
+/**
+ * Installation for Mac - nearly identical to that for Linux.
+ * @since 1.0.0
+ */
 public class MacInstaller extends Installer {
+
+    /**
+     * Same as Linux
+     */
+    public MacInstaller() {
+        steps = 4;
+    }
+
+    /**
+     * Install in home directory.
+     * @param version which version
+     */
     @Override
     public void install(String version) {
-        System.out.println("Installing for mac");
-        super.coreInstall(version, System.getProperty("user.home"));
+        System.out.println("Installing for mac ...");
+        coreInstall(version, System.getProperty("user.home"));
     }
 
     @Override
     public boolean isInstalled(String version) {
-        return false;
+        return new File(
+                System.getProperty("user.home") + "/.qme/"
+                        + version + "/" + version + ".jar"
+        ).exists();
     }
 
     @Override
